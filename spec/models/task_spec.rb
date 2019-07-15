212d53dbc1b56e59fecb362015c67fb6a34f8f41
evansun <pipltbtb@gmail.com>
2019-07-15 15:37:33 +0800

    add task model validate

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+
+  it "不能沒有標題及內文" do
+    expect(FactoryBot.build(:task, title: nil, content: nil)).not_to be_valid
+  end
+
+  it "不能沒有標題" do
+    expect(FactoryBot.build(:task, title: nil, content: 'it is content')).not_to be_valid
+  end
+
+  it "不能沒有內文" do
+    expect(FactoryBot.build(:task, title: 'it is title', content: nil)).not_to be_valid
+  end
 end
