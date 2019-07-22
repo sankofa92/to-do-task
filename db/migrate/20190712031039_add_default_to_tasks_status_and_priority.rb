2f2a976e1591ab6c4873cb15db4b36b6fe68a1e5
evansun <pipltbtb@gmail.com>
2019-07-22 19:14:23 +0800

    test CI v5

--- db/migrate/20190712031039_add_default_to_tasks_status_and_priority.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultToTasksStatusAndPriority < ActiveRecord::Migration[5.2]
-  def change
-    change_column :tasks, :status, :string, default: 'pending', null: false
-    change_column :tasks, :priority, :string, default: 'low', null: false
-  end
-end
