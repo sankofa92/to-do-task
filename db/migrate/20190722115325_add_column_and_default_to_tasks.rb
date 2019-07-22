3c1ea1c93cd866ee2a0493b9628b923f75095642
evansun <pipltbtb@gmail.com>
2019-07-22 19:55:16 +0800

    test CI v6

--- db/migrate/20190722115325_add_column_and_default_to_tasks.rb
+++ db/migrate/20190722115325_add_column_and_default_to_tasks.rb
@@ -1,4 +1,4 @@
-class AddColumnTypeToTasks < ActiveRecord::Migration[5.2]
+class AddColumnAndDefaultToTasks < ActiveRecord::Migration[5.2]
   def change
     add_column :tasks, :status, :integer, default: 0, null: false
     add_column :tasks, :priority, :integer, default: 0, null: false
