473032186901ab6a1c34b90470925a04c7ee629d
evansun <pipltbtb@gmail.com>
2019-07-23 14:16:30 +0800

    add search index for tasks title

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   include AASM
   # belongs_to :user
-  has_and_belongs_to_many :tags
+  has_and_belongs_to_many :tags, counter_cache: true
   
   validates :title, presence: true
   validates :content, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_115325) do
+ActiveRecord::Schema.define(version: 2019_07_23_052058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_07_22_115325) do
     t.datetime "updated_at", null: false
     t.integer "status", default: 0, null: false
     t.integer "priority", default: 0, null: false
+    t.index ["title"], name: "index_tasks_on_title"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
