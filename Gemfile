0de496473429569be8318f6f8718b0f4437a3e79
evansun <pipltbtb@gmail.com>
2019-07-29 16:22:26 +0800

    gem bcrypt, set password digest

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem 'bootstrap', '~> 4.3', '>= 4.3.1'
 gem 'paranoia', '~> 2.4', '>= 2.4.2'
 gem 'rails-i18n', '~> 5.1', '>= 5.1.3'
 gem 'ransack', '~> 2.1', '>= 2.1.1'
+gem 'bcrypt', '~> 3.1', '>= 3.1.13'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_152823) do
+ActiveRecord::Schema.define(version: 2019_07_29_081038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_07_28_152823) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "tasks_count", default: 0
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
