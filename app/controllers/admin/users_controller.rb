adab5ab740e50d05e7a5646a042bbb17f5c8cd97
evansun <pipltbtb@gmail.com>
2019-08-18 17:49:04 +0800

    add admin create user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,30 @@
 class Admin::UsersController < ApplicationController
-
+  before_action :find_user, only: [:show, :edit, :update, :destrroy]
   def index
     @users = User.order(created_at: :desc).page(params[:page]).per(10)
   end
   
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: I18n.t('users.notice.create')
+    else
+      render :new, alert: I18n.t('users.alert.create')
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by(id: params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
